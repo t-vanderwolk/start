69154da7da7b45845906ef6474cd962a67718800
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2022-07-25 10:41:50 -0700

    fetching from 3rd party api using a query

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@
 import './App.css';
 import { useState, useEffect} from "react";
 
+const query = `
+query {
+  allLifts{
+    name
+    elevationGain
+    status
+  }
+}`;
 
-function GithubUser({name, location, avatar}) {
+const opts = {
+  method: "POST",
+  headers: {"Content-Type": "application/json"},
+  body: JSON.stringify({query})
+};
+
+function Lift({name, elevationGain, status}) {
   return(
     <div>
       <h1>{name}</h1>
-      <p>{location}</p>
-      <img src={avatar}  alt={name} />
+      <p>{elevationGain} {status}</p>
+ 
     </div>
   );
 }
@@ -22,7 +36,7 @@ const [loading, setLoading] = useState(false);
 
 useEffect(() => {
   setLoading(true);
-  fetch('https://api.github.com/users/t-vanderwolk')
+  fetch('https://snowtooth.moonhighway.com/', opts)
   .then((response) => response.json())
   .then(setData)
   .then(() => setLoading(false))
@@ -33,11 +47,15 @@ if (loading) return <h1>Loading...</h1>;
 if (error) return <pre>{JSON.stringify(error)}</pre>;
 if(!data) return null;
 return(
-    <GithubUser 
-    name={data.login} 
-    location={data.location}
-    avatar={data.avatar_url}
-    />
+ <div>
+  {data.data.allLifts.map((lift) => (
+    <Lift
+      name={lift.name}
+      elevationGain={lift.elevationGain}
+      status={lift.status}
+       />
+  ))}
+ </div>
  
 );
 
