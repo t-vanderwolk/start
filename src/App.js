0e9a7120803d3274d13a4e54cd1b939800a875e2
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2022-07-26 13:55:42 -0700

    displaying information using the Outlet component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 function Home() {
   return (
@@ -24,8 +24,17 @@ export function About() {
         <Link to="/contact">Contact Us</Link>
         </nav>
       <h1>About Us</h1>
+      <Outlet />
     </div>
     
+  );
+}
+
+export function History() {
+  return (
+    <div>
+      <h1>Our History</h1>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { App, About, Contact } from "./App";
+import { App, About, Contact, History } from "./App";
 import {
   BrowserRouter,
   Routes,
@@ -12,7 +12,9 @@ ReactDOM.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="/about" element={<About />} />
+      <Route path="/about" element={<About />}>
+        <Route path="history" element={<History />} />
+      </Route>
       <Route
         path="/contact"
         element={<Contact />}
